78cfa0864e0e8264bd5a640ba3f758e7508bcd90
Timothy Miller <tmiller@sailab.com>
2016-07-28 14:51:35 -0400

    Chapter 8 Complete.

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,12 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     // Getters and Setters.
@@ -19,4 +23,11 @@ public class Crime {
 
     public String getTitle() { return mTitle; }
     public void setTitle(String title) { mTitle = title; }
+
+    public boolean isSolved() { return mSolved; }
+    public void setSolved(boolean solved) { mSolved = solved; }
+
+    public void setDate(Date date) { mDate = date; }
+    public Date getDate() { return mDate; }
+
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -16,6 +20,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     /*
         Overridden (Inherited) methods
@@ -28,23 +34,42 @@ public class CrimeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // inflate the view.
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
+        // get references to widgets in the view that was just inflated.
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+
+        // add listeners where appropriate.
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
                 // do nothing.
             }
-
             @Override
             public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
                 mCrime.setTitle(charSequence.toString());
             }
-
             @Override
             public void afterTextChanged(Editable editable) {
                 // do nothing.
+            }
+        });
+
+        // setup the date button.
+        DateFormat df = new DateFormat();
+        mDateButton.setText(df.format("MMMM dd, yyyy", mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        // setup the "solved" check box.
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                // Set the crime's solved property.
+                mCrime.setSolved(isChecked);
             }
         });
 
