0cb0985460415950e1ae4a7d6f5afc13e5f46edd
Timothy Miller <timothy.miller.cs@gmail.com>
2016-07-29 11:20:06 -0400

    AddingNewFragment
    Adding CrimeListActivity and CrimeListFragment.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by TMiller on 7/29/2016.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by TMiller on 7/29/2016.
+ */
+public class CrimeListFragment extends Fragment {
+
+
+
+}
