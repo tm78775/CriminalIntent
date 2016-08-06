3467c687f93a39232cb79b066731312ab49f0f8f
Timothy Miller <timothy.miller.cs@gmail.com>
2016-08-05 23:13:20 -0400

    Pushing partial Ch13 code - fully WORKING. Forgot to create a new branch before coding.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.criminalintent.database;
+
+/**
+ * Created by Timothy on 8/5/16.
+ */
+public class CrimeBaseHelper {
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalintent.database;
+
+/**
+ * Created by Timothy on 8/5/16.
+ */
+public class CrimeDbSchema {
+
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+    }
+
+    public static final class Cols {
+        public static final String UUID = "uuid";
+        public static final String TITLE = "title";
+        public static final String DATE = "date";
+        public static final String SOLVED = "solved";
+    }
+
+}
